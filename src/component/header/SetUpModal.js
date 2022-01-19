62556ccc79ce29374fbc7715efe271e9b703871c
Kim Nahyeong <of.kimna@gmail.com>
2022-01-19 17:06:11 +0900

    feat(SetUpModal): Wallet Button Image added #3

--- src/component/header/SetUpModal.js
+++ src/component/header/SetUpModal.js
@@ -53,8 +53,20 @@ const ModalContent = styled.div`
         border-radius: 5px;
         border: none;
         margin-bottom: 23px;
-        font-size: 1rem;
+        font-size: 1.2rem;
         cursor: pointer;
+        text-align: lieft;
+
+        img{
+            width: 23px;
+            height: 23px;
+            margin: 0 10px 0 0;
+            vertical-align: middle;
+            float: left;
+        }
+        span{
+            margin-left: -18px;
+        }
     }
 `
 
@@ -99,9 +111,18 @@ export const SetUpModal = ({showModal, setShowModal, setWallet}) => {
                         <ModalContent>
                             <h1>Connect Wallet</h1>
                             <p>Select Wallet to Connect with ARTISTELLA</p>
-                            <button onClick={() => connectWallet("phantom", setShowModal, setWallet)}>Phantom</button>
-                            <button onClick={() => connectWallet("slope", setShowModal, setWallet)}>Slope</button>
-                            <button onClick={() => connectWallet("solFlare", setShowModal, setWallet)}>SolFlare</button>
+                            <button onClick={() => connectWallet("phantom", setShowModal, setWallet)}>
+                                <img src="https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=2,format=auto/https%3A%2F%2Fdocs.phantom.app%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-28427.appspot.com%2Fo%2Fspaces%252F-MVOiF6Zqit57q_hxJYp%252Favatar-1615495356537.png%3Fgeneration%3D1615495356841399%26alt%3Dmedia"/>
+                                <span>Phantom</span>
+                            </button>
+                            <button onClick={() => connectWallet("slope", setShowModal, setWallet)}>
+                                <img src="https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=2,format=auto/https%3A%2F%2Fdocs.slope.finance%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252F-MlOlWuD8X-mIrzvVU3W%252Ficon%252FQ8tdcABNleXu7Spqfdqy%252F222.png%3Falt%3Dmedia%26token%3D3f599864-00d3-4b53-8fe3-0b1f93d2bec4"/>
+                                <span>Slope</span>
+                            </button>
+                            <button onClick={() => connectWallet("solFlare", setShowModal, setWallet)}>
+                                <img src="https://www.gitbook.com/cdn-cgi/image/width=40,height=40,fit=contain,dpr=2,format=auto/https%3A%2F%2Fdocs.solflare.com%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252F-Mgv3_8586v0mVL4zZax%252Ficon%252FWvDTo0Kodwa4awPEQrsO%252Fflarelogo.png%3Falt%3Dmedia%26token%3Db115a1f0-a543-4a49-828a-e3ede684055e"/>
+                                <span>SolFlare</span>
+                            </button>
                         </ModalContent>
                         <CloseModalButton aria-label='Close modal' 
                             onClick={() => setShowModal(prev => !prev)}/>
